532b4167f79f9ae6c613f963cac4fd2131b7ddc0
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-04 15:36:23 +0100

    scalar v1

---
+++ scalar/target/debug/deps/scalar-2742f0301f38de46.d
@@ -0,0 +1,5 @@
+/home/bdouirat/Desktop/rust-piscine/scalar/target/debug/deps/scalar-2742f0301f38de46: src/main.rs
+
+/home/bdouirat/Desktop/rust-piscine/scalar/target/debug/deps/scalar-2742f0301f38de46.d: src/main.rs
+
+src/main.rs:
---
+++ scalar/target/debug/scalar.d
@@ -0,0 +1,1 @@
+/home/bdouirat/Desktop/rust-piscine/scalar/target/debug/scalar: /home/bdouirat/Desktop/rust-piscine/scalar/src/main.rs
