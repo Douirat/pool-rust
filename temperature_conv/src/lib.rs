1487365ce0b6d8c66b43ecdea7c470f8b90a79fc
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-04 16:14:46 +0100

    temprature conversion v2

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let result: f64 = (32 - 32) * 9 / 5
+    let result: f64 = (f - 32) * 9.0 / 5;
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let result: f64 = (c * 9/5) + 32
+    let result: f64 = (c * 9.0/5) + 32;
 }
