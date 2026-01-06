03e12a1a115209d409125de21bec543257b60528
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-06 10:19:22 +0100

    struct operation

---
+++ changes/src/lib.rs
@@ -0,0 +1,20 @@
+#[dervie(Debug, PartialEq, Eq, Clone)]
+struct Light{
+    pub alias: String,
+    pub brightness: u8,
+}
+impl Light {
+	 fn new(alias: &str) -> Self {
+    Light{
+    alias: String::from(alias),
+	brightness:0,
+    }
+	}
+    }
+     fn change_brightness(lights: &mut [Light], alias: &str, value: u8) {
+    for light in lights {
+        if light.alias == alias{
+        light.brightness = value;
+        }
+    }
+    }
