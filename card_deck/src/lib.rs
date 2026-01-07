71dce378299838379d64175baaaa7d29c51f5546
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-07 16:04:15 +0100

    card_deck v3

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,6 +1,5 @@
-use rand::Rng;
+use rand::prelude::*;
 
-// ---------------- Suit Enum ----------------
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Suit {
     Heart,
@@ -10,7 +9,7 @@ pub enum Suit {
 }
 
 impl Suit {
-    // Translate u8 -> Suit
+
     pub fn translate(value: u8) -> Suit {
         match value {
             1 => Suit::Heart,
@@ -21,16 +20,16 @@ impl Suit {
         }
     }
 
-    // Generate random Suit
+
     pub fn random() -> Suit {
-        let mut rng = rand::rng();
-        let x = rng.random_range(1..=4);
+        let mut rng = thread_rng();
+        let x = rng.gen_range(1..=4);
         Suit::translate(x)
     }
 }
 
-// ---------------- Rank Enum ----------------
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Rank {
     Ace,
     Number(u8), // 2..10
@@ -40,7 +39,7 @@ pub enum Rank {
 }
 
 impl Rank {
-    // Translate u8 -> Rank
+
     pub fn translate(value: u8) -> Rank {
         match value {
             1 => Rank::Ace,
@@ -52,38 +51,23 @@ impl Rank {
         }
     }
 
-    // Generate random Rank
+
     pub fn random() -> Rank {
-        let mut rng = rand::rng();
-        let x = rng.random_range(1..=13);
+        let mut rng = thread_rng();
+        let x = rng.gen_range(1..=13);
         Rank::translate(x)
     }
 }
 
-// ---------------- Card Struct ----------------
+
 #[derive(Debug)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
-// ---------------- winner_card Function ----------------
+
 pub fn winner_card(card: &Card) -> bool {
     card.rank == Rank::Ace && card.suit == Suit::Spade
 }
 
-// ---------------- Main Function (Example Usage) ----------------
-fn main() {
-    let your_card = Card {
-        rank: Rank::random(),
-        suit: Suit::random(),
-    };
-
-    println!("Your card is {:?}", &your_card);
-
-    if winner_card(&your_card) {
-        println!("You are the winner!");
-    } else {
-        println!("Try again!");
-    }
-}
