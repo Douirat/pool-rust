91f218928c677e50c562e03df17bec764f752e04
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-07 11:35:38 +0100

    circle v2

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -5,22 +5,22 @@ pub struct Circle {
 }
 
 impl Circle {
-fn new(x: f64, y:f64, radius: f64)-> Circle {
+pub fn new(x: f64, y:f64, radius: f64)-> Circle {
     Circle{
         center: Point(x, y),
         radius: radius
     }
 }
  
- fn area(&self) -> f64{
+ pub fn area(&self) -> f64{
      let pi = 3.14159;
      pi*(self.radius * self.radius)
  }   
     
-    fn diameter(&self) -> f64{
+    pub fn diameter(&self) -> f64{
         2.0*self.radius
     }
-    fn intersect(&self, other: Circle) ->bool{
+    pub fn intersect(&self, other: Circle) ->bool{
         let d = self.center.distance(other.center);
         self.radius - other.radius < d && d < self.radius - other.radius  
     }
@@ -30,7 +30,7 @@ fn new(x: f64, y:f64, radius: f64)-> Circle {
 pub struct Point(f64, f64);
 
 impl Point {
-fn distance(&self, other: Point)->f64{
+pub fn distance(&self, other: Point)->f64{
     let dx = other.0 - self.0;
     let dy = other.1 - self.1;
     (dx*dx + dy *dy).sqrt()
