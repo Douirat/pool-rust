fc702797f1f5856f557b455db9e6637fccc1f73b
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-07 11:41:18 +0100

    circle v3

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -27,7 +27,7 @@ pub fn new(x: f64, y:f64, radius: f64)-> Circle {
 }
 
 #[derive(Debug, Clone, Copy)]
-pub struct Point(f64, f64);
+pub struct Point(pub f64,pub f64);
 
 impl Point {
 pub fn distance(&self, other: Point)->f64{
