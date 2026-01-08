3a6c396531ca1fda0df2ea27671e934ac0711f04
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-08 17:25:54 +0100

    hash

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -1,22 +1,35 @@
+
+use std::collections::HashMap;
+
 pub fn mean(list: &[i32]) -> f64 {
     let sum: i32 = list.iter().sum();
     sum as f64 / list.len() as f64
 }
 
 pub fn median(list: &[i32]) -> i32 {
-    let mut v = list.to_vec();
-    v.sort();
-
-    let mid = v.len() / 2;
-
-    if v.len() % 2 == 1 {
-        v[mid]
-    } else {
-        (v[mid - 1] + v[mid]) / 2
-    }
+let mut arr:Vec<i32> = Vec::new();
+for i in list.iter(){
+    arr.push(*i);
+}
+bubble_sort(&mut arr);
+if arr.len() % 2 == 0{
+   (arr[arr.len() / 2 as usize] +  arr[(arr.len() / 2 as usize) + 1] ) / 2
+} else{
+     arr[arr.len() / 2 as usize]
+}
 }
 
-use std::collections::HashMap;
+
+fn bubble_sort(arr: &mut Vec<i32>) {
+    let n = arr.len();
+    for i in 0..n {
+        for j in 0..n - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
 
 pub fn mode(list: &[i32]) -> i32 {
     let mut freq = HashMap::new();
