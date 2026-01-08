2c52509c1470f8b74418c57d73be9636c6927fce
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-08 15:57:16 +0100

    hash map 1

--- bigger/src/lib.rs
+++ bigger/src/lib.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 pub fn bigger(h: HashMap<&str, i32>) -> i32 {
-    let mut result = i32::MAX;
+    let mut result = i32::MIN;
     for (_, value) in *h{
-        if value < result;
+        if value > result;
         result = value;
     }
     // *h.values().max().unwrap_or(&i32::MAX)
