ab3de2fd3adfd2aa0ce2c41368f3570a3da58c71
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-08 15:45:50 +0100

    hash map

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut map = HashMap::new();
+
+    for &word in words {
+        // entry() lets us update or insert in one step
+        *map.entry(word).or_insert(0) += 1;
+    }
+
+    map
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
+}
