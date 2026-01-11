c5ace5647376f4a6d28b74959e3b3fe988f5eb60
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-11 16:51:20 +0100

    handle

--- tester/target/debug/deps/tester-597db684e0ffef5e.d
+++
@@ -1,5 +0,0 @@
-/home/bdouirat/Desktop/work/piscine-rust/tester/target/debug/deps/tester-597db684e0ffef5e.d: src/main.rs
-
-/home/bdouirat/Desktop/work/piscine-rust/tester/target/debug/deps/tester-597db684e0ffef5e: src/main.rs
-
-src/main.rs:
--- tester/target/debug/tester.d
+++
@@ -1,1 +0,0 @@
-/home/bdouirat/Desktop/work/piscine-rust/tester/target/debug/tester: /home/bdouirat/Desktop/work/piscine-rust/tester/src/main.rs
