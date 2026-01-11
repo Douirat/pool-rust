37acb34e0ecbbec56abe668f14d0d954fe26ed30
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-11 14:59:32 +0100

    error handling in rust v1

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -7,14 +7,17 @@ pub enum Security {
 }
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
-    match security_level {
+    match security_level{
         Security::Unknown => server.unwrap().to_string(),
         Security::Message => server.expect("ERROR: program stops").to_string(),
         Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
-        Security::NotFound => server.unwrap_or_else(|e| format!("Not found: {}", e)),
-        Security::UnexpectedUrl => match server {
-            Ok(url) => panic!("{}", url), 
-            Err(e) => e.to_string(),      
-        },
+        Security::NotFound => match server {
+            Ok(val) => val.to_string(),
+            Err(e) => format!("Not found: [{}]", e),
+            },
+        Security::UnexpectedUrl =>  match server {
+                Ok(value) => panic!(value),
+                Err(e) => e.to_string(),
+            },
     }
 }
