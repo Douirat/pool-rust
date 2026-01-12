639d102267fe30a4ae93e0b678aaa825bf54a755
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-12 11:45:13 +0100

    error

---
+++ error_types/src/lib.rs
@@ -0,0 +1,60 @@
+use chrono::Local; // for timestamp
+use std::collections::HashMap;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: HashMap<&'static str, String>,
+    pub date: String,
+    pub err: &'static str,
+}
+
+impl FormError {
+    pub fn new(field_name: &'static str, field_value: String, err: &'static str) -> Self {
+        let mut values = HashMap::new();
+        values.insert(field_name, field_value);
+
+        Self {
+            form_values: values,
+            date: Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            err,
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn validate(&self) -> Result<(), FormError> {
+        // Validate name
+        if self.name.is_empty() {
+            return Err(FormError::new("first_name", self.name.clone(), "Username is empty"));
+        }
+
+        // Validate password length
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+
+        let has_letter = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_number = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| !c.is_ascii_alphanumeric());
+
+        if !has_letter || !has_number || !has_symbol {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII numbers, letters and symbols",
+            ));
+        }
+
+        Ok(())
+    }
+}
