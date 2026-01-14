5792c411e34225d57d790fd6cb0d7d124cefdb1a
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-14 09:55:01 +0100

    string repeat v3

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,8 +1,10 @@
 pub fn stars(n: u32) -> String {
     let mut x:usize = 2;
-    
+    if n == 0 {
+        x = 1;
+    }
     for i in 1..n{
-        x *= 2
+        x *= 2;
     }
    
 "*".to_string().repeat(x)
