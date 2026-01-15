95beff3c454b1bd80dd5a4bcda17cf056e033aaa
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-15 14:33:30 +0100

    swap

--- rgb_match/src/lib.rs
+++ rgb_match/src/lib.rs
@@ -8,35 +8,21 @@ pub struct Color {
 
 impl Color {
     pub fn swap(self, first: u8, second: u8) -> Color {
-        let Color { r, g, b, a } = self;
+        let mut ch = [self.r, self.g, self.b, self.a];
 
-        match (r, g, b, a) {
-            // r <-> g
-            (f, s, b, a) if f == first && s == second => Color { r: s, g: f, b, a },
-            (s, f, b, a) if s == first && f == second => Color { r: f, g: s, b, a },
-
-            // r <-> b
-            (f, g, s, a) if f == first && s == second => Color { r: s, g, b: f, a },
-            (s, g, f, a) if s == first && f == second => Color { r: f, g, b: s, a },
-
-            // r <-> a
-            (f, g, b, s) if f == first && s == second => Color { r: s, g, b, a: f },
-            (s, g, b, f) if s == first && f == second => Color { r: f, g, b, a: s },
-
-            // g <-> b
-            (r, f, s, a) if f == first && s == second => Color { r, g: s, b: f, a },
-            (r, s, f, a) if s == first && f == second => Color { r, g: f, b: s, a },
-
-            // g <-> a
-            (r, f, b, s) if f == first && s == second => Color { r, g: s, b, a: f },
-            (r, s, b, f) if s == first && f == second => Color { r, g: f, b, a: s },
-
-            // b <-> a
-            (r, g, f, s) if f == first && s == second => Color { r, g, b: s, a: f },
-            (r, g, s, f) if s == first && f == second => Color { r, g, b: f, a: s },
-
-            // nothing matched → unchanged
-            _ => self,
+        let mut i = None;
+        let mut j = None;
+        for (idx, &val) in ch.iter().enumerate() {
+            if val == first  && i.is_none() { i = Some(idx); }
+            if val == second && j.is_none() { j = Some(idx); }
         }
+
+        if let (Some(i), Some(j)) = (i, j) {
+            if i != j {
+                ch.swap(i, j);
+            }
+        }
+
+        Color { r: ch[0], g: ch[1], b: ch[2], a: ch[3] }
     }
 }
