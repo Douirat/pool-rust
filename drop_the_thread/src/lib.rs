eba59a45ec7644176120e3449e5988c98e7461bb
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-19 13:52:40 +0100

    thread

---
+++ drop_the_thread/src/lib.rs
@@ -0,0 +1,64 @@
+use std::cell::{Cell, RefCell};
+
+#[derive(Debug)]
+pub struct ThreadPool {
+    pub drops: Cell<usize>,
+    pub states: RefCell<Vec<bool>>
+}
+
+impl ThreadPool {
+    pub fn new() -> Self {
+       Self{
+        drops: Cell::new(0),
+        states: RefCell::new(Vec::new()),
+    }
+}
+
+    pub fn new_thread(&self, c: String) -> (usize, Thread) {
+       let id = self.thread_len();
+       self.states.borrow_mut().push(false);
+       (id, Thread::new(id, c, self))
+    }
+
+    pub fn thread_len(&self) -> usize {
+        self.states.borrow().len()
+    }
+
+    pub fn is_dropped(&self, id: usize) -> bool {
+        *self.states.borrow().get(id).unwrap_or(&false)
+    }
+
+    pub fn drop_thread(&self, id: usize) {
+        let mut states = self.states.borrow_mut();
+        let t = states.get_mut(id).expect("invalid thred id");
+        if *t{
+            panic!("X is already dropped");
+        }
+        *t = true;
+        self.drops.set(self.drops.get() + 1);
+    }
+}
+
+#[derive(Debug)]
+pub struct Thread<'a> {
+   pid: usize,
+   cmd: String,
+   parent: &'a ThreadPool,
+
+}
+
+impl<'a> Thread<'a> {
+    pub fn new(p: usize, c: String, t: &'a ThreadPool) -> Self {
+        Self{pid: p, cmd: c, parent: t}
+    }
+
+    pub fn skill(mut self) {
+       drop(&mut self)
+    }
+}
+
+impl Drop for Thread<'_> {
+    fn drop(&mut self){
+        self.parent.drop_thread(self.pid)
+    }
+}
