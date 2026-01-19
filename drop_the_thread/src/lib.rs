93df70102e22254f8876a71835e0f814cef1eff2
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-19 14:17:18 +0100

    thread v1

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -25,7 +25,7 @@ impl ThreadPool {
     }
 
     pub fn is_dropped(&self, id: usize) -> bool {
-        *self.states.borrow().get(id).copied().unwrap_or(&false)
+        self.states.borrow().get(id).copied().unwrap_or(false)
     }
 
     pub fn drop_thread(&self, id: usize) {
