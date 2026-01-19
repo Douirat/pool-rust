ab06c4958b7af898cc326b8b267e3ad1f1edb408
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-19 14:22:03 +0100

    thread v1

--- drop_the_thread/src/lib.rs
+++ drop_the_thread/src/lib.rs
@@ -41,9 +41,9 @@ impl ThreadPool {
 
 #[derive(Debug)]
 pub struct Thread<'a> {
-   pid: usize,
-   cmd: String,
-   parent: &'a ThreadPool,
+   pub pid: usize,
+   pub cmd: String,
+   pub parent: &'a ThreadPool,
 
 }
 
