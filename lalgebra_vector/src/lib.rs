bf00fcd49c9f35796d1f591ec24f3b4c5404646f
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-22 16:40:57 +0100

    g

--- lalgebra_vector/src/lib.rs
+++ lalgebra_vector/src/lib.rs
@@ -1,4 +1,30 @@
-use std::ops::Add;
+use std::ops::{Add, Mul};
+
+/// Scalar trait
+pub trait Scalar:
+    Copy
+    + Add<Output = Self>
+    + Mul<Output = Self>
+    + Default
+    + PartialEq
+    + std::fmt::Debug
+{
+}
+
+/// Blanket implementation
+impl<T> Scalar for T
+where
+    T: Copy
+        + Add<Output = T>
+        + Mul<Output = T>
+        + Default
+        + PartialEq
+        + std::fmt::Debug,
+{
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Vector<T: Scalar>(pub Vec<T>);
 
 impl<T: Scalar> Add for Vector<T> {
     type Output = Option<Vector<T>>;
@@ -16,5 +42,21 @@ impl<T: Scalar> Add for Vector<T> {
             .collect();
 
         Some(Vector(data))
+    }
+}
+
+impl<T: Scalar> Vector<T> {
+    pub fn dot(self, rhs: Self) -> Option<T> {
+        if self.0.len() != rhs.0.len() {
+            return None;
+        }
+
+        let mut sum = T::default();
+
+        for (a, b) in self.0.into_iter().zip(rhs.0.into_iter()) {
+            sum = sum + a * b;
+        }
+
+        Some(sum)
     }
 }
