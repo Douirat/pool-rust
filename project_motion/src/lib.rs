74dcd2aa8d8bae55d50933d4398e0d8eeaf68531
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-29 16:47:20 +0100

    step

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -54,3 +54,34 @@ impl Iterator for ThrowObject {
         Some(self.clone())
     }
 }
+impl Iterator for ThrowObject {
+    type Item = ThrowObject;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let gravity = -9.8;
+
+        // Increment time by 1 second
+        self.time += 1.0;
+
+        // Compute new position
+        let new_x = self.init_position.x + self.init_velocity.x * self.time;
+        let new_y = self.init_position.y + self.init_velocity.y * self.time + 0.5 * gravity * self.time * self.time;
+
+        // Compute new velocity
+        let new_vx = self.init_velocity.x;
+        let new_vy = self.init_velocity.y + gravity * self.time;
+
+        // If object has hit the floor (y <= 0), return None
+        if new_y <= 0.0 {
+            return None;
+        }
+
+        // Round to 1 decimal place
+        let round1 = |val: f32| (val * 10.0).round() / 10.0;
+
+        self.actual_position = Object { x: round1(new_x), y: round1(new_y) };
+        self.actual_velocity = Object { x: round1(new_vx), y: round1(new_vy) };
+
+        Some(self.clone())
+    }
+}
