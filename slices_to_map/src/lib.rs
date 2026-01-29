c48b53f8bde45fbf98c341b3140286ccdabcbaa2
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-29 16:41:36 +0100

    slice to map

---
+++ slices_to_map/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+pub fn slices_to_map<'a, T: Eq + Hash, U>(
+    keys: &'a [T],
+    values: &'a [U],
+) -> HashMap<&'a T, &'a U> {
+    let mut map = HashMap::new();
+    let len = keys.len().min(values.len());
+
+    for i in 0..len {
+        map.insert(&keys[i], &values[i]);
+    }
+
+    map
+}
