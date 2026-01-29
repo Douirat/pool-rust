ab6622cf7d3529c8b5a007d802c123b898a75a3f
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-29 15:27:36 +0100

    adding

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,6 +1,8 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
     let mut result = Vec::new();
-    
+    if arr.len() == 1 {
+         return arr;
+    }
     for i in 0..arr.len() {
         let mut prod = 1;
         for j in 0..arr.len() {
